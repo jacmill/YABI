361343016c37380f4520e7388f66422bf59a5fe9
jacmill <jakub3020.jmy@gmail.com>
2022-07-16 23:45:35 +0200

    Created token struct and token type enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+struct Token {
+    value: char,
+    type_: TokenType,
+    pos: u32 
+}
+
+enum TokenType {
+    MovePointerRight,
+    MovePointerLeft,
+    IncrementCell,
+    DecrementCell,
+    OutputCell,
+    InputCell,
+    BeginLoop,
+    EndLoop
+}
 fn main() {
     println!("Hello, world!");
 }
